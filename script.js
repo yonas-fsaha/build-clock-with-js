46f30f23b19c70a3c4e14f3883a5a968be9453d5
Yonas Fsaha <umyonas7@gmail.com>
2023-01-26 14:14:40 +0300

    build-clock-with-js inital commit

---
+++ script.js
@@ -0,0 +1,30 @@
+setInterval(setClock, 1000)
+
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+
+    const currentDate = new Date()
+
+    const secondsRatio = currentDate.getSeconds() / 60
+
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(minuteHand, minutesRatio)
+    setRotation(secondHand, secondsRatio)
+    setRotation(hourHand, hoursRatio)
+
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+
+// element.style means refers that element like document....
+//
