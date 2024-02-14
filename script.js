1a8ad1907582dec2ba2043fca39374c176552e51
yona404 <104067688+yonas-fsaha@users.noreply.github.com>
2024-02-14 20:48:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setInterval(setClock, 1000)
 
 
-const hourHand = document.querySelector('[data-hour-hand]')
+const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
